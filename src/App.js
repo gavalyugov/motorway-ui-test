565d9f5e43eb84aa515e1109a09c8aae5fdcdd0a
Simeon Gavalyugov <simeon@abundanceinvestment.com>
2023-08-29 14:20:51 +0100

    Add an alt description

--- src/App.js
+++ src/App.js
@@ -21,7 +21,11 @@ const App = () => {
       <div className="timeline">
         {images?.map((img) => (
           <div className="item" key={img.id}>
-            <img src={`${img.url}.jpg`} alt="" className="car" />
+            <img
+              src={`${img.url}.jpg`}
+              alt={img.alt_description}
+              className="car"
+            />
             <img
               className="user"
               src={`${img.user.profile_image}.webp`}
