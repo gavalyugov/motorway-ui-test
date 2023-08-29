2566f0974eb5570dd1e049b2967c106effec2035
Simeon Gavalyugov <simeon@abundanceinvestment.com>
2023-08-29 14:16:27 +0100

    Add a timeline

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [images, setImages] = useState();
 
   useEffect(() => {
-    fetch('images?limit=10')
-      .then(res => res.json())
-      .then(data => {
-        console.log('Success:', data);
+    fetch("images?limit=10")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Success:", data);
         setImages(data);
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.error("Error:", error);
       });
   }, []);
 
   return (
-    <div className='app grid'>
-      {
-         images?.map(img => (
-          <div key={img.id}>
-            <img src={`${img.url}.jpg`} alt='' className='car'/>
-            <img src={`${img.user.profile_image}.webp`} alt='' className='user'/>
+    <div className="content">
+      <div className="timeline">
+        {images?.map((img) => (
+          <div className="item" key={img.id}>
+            <img src={`${img.url}.jpg`} alt="" className="car" />
+            <img
+              className="user"
+              src={`${img.user.profile_image}.webp`}
+              alt=""
+            />
           </div>
-        ))
-      }
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
