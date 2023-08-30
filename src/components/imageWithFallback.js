6fb809d7bc6c8e6871111d4456e30aa71f6cbaa3
Simeon Gavalyugov <simeon@abundanceinvestment.com>
2023-08-30 08:59:45 +0100

    Fix prop name

--- src/components/imageWithFallback.js
+++ src/components/imageWithFallback.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const ImageWithFallback = ({ imageURL, alt, className }) => {
   return (
-    <picture className="className">
-      <source srcset={`${imageURL}.webp`} type="image/webp" />
-      <source srcset={`${imageURL}.jpg`} type="image/jpeg" />
+    <picture>
+      <source srcSet={`${imageURL}.webp`} type="image/webp" />
+      <source srcSet={`${imageURL}.jpg`} type="image/jpeg" />
       <img className={className} src={`${imageURL}.jpg`} alt={alt} />
     </picture>
   );
